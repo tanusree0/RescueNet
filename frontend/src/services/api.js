c5f3195ef7d70d2b1fff4b03f39a7c28564c3d89
tanusree0 <tanusree6060@gmail.com>
2026-03-14 01:20:43 +0530

    connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://rescuenet-k0jc.onrender.com/api' });
 API.interceptors.request.use((req) => {
   const profile = localStorage.getItem('userProfile');
   if (profile) req.headers.Authorization = `Bearer ${JSON.parse(profile).token}`;
